ed1703db3f708b65d8fe9959a464ee476f44e7fc
Luan Mai <luan.mai@lonsec.com.au>
2015-08-18 17:36:06 +1000

    initial import

---
+++ app.py
@@ -0,0 +1,17 @@
+__author__ = 'LMai'
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
